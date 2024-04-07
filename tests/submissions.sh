06d9d76ffc115126c39d7b29dd456ffdb4a2921a
Brian Kurilko <briankurilko@gmail.com>
2024-04-07 12:21:13 -0400

    Got pipeline working, made test script remove pubsub message off of test topic

--- tests/submissions.sh
+++ tests/submissions.sh
@@ -1,8 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 topicName="test-topic"
+subscription_name="test-subscription"
 if ! gcloud pubsub topics create "$topicName"; then
     echo "Topic $topicName already exists"
 fi
 
-gcloud builds submit . --config cloudbuild.yaml --substitutions "_REPO_TO_CLONE=https://github.com/brianpipeline/test-cloudbuild.git,_REPLY_TOPIC=\"projects/cloud-build-pipeline-396819/topics/$topicName\"" --region=us-central1
+if ! gcloud pubsub subscriptions create "$subscription_name" --topic "$topicName"; then
+    echo "Subscription $subscription_name already exists"
+fi
+
+gcloud builds submit . --config cloudbuild.yaml --substitutions "_REPO_TO_CLONE=https://github.com/brianpipeline/test-cloudbuild.git,_REPO_NAME="test-cloudbuild",_REPLY_TOPIC=\"projects/cloud-build-pipeline-396819/topics/$topicName\"" --region=us-central1
+
+message=$(gcloud pubsub subscriptions pull --auto-ack "$subscription_name" --format='value(message.data)' 2>/dev/null)
+if [[ -n $message ]]; then
+    echo "Received Message: $message"
+    return 0
+else
+    return 1
+fi
