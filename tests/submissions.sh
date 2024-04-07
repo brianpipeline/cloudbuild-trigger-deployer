2d520c8ae1a2b82628e116480710a272960d3bfd
Brian Kurilko <briankurilko@gmail.com>
2024-04-07 10:45:05 -0400

    Updated cloud build trigger thing

--- tests/submissions.sh
+++ tests/submissions.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-gcloud builds submit . --config cloudbuild.yaml --region=us-central1
+topicName="test-topic"
+if ! gcloud pubsub topics create "$topicName"; then
+    echo "Topic $topicName already exists"
+fi
+
+gcloud builds submit . --config cloudbuild.yaml --substitutions "_REPO_TO_CLONE=https://github.com/brianpipeline/test-cloudbuild.git,_REPLY_TOPIC=\"projects/cloud-build-pipeline-396819/topics/$topicName\"" --region=us-central1
